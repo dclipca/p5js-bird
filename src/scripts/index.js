efbefdc4548a2ff7a2f651eb321a449e0eb6f7b6
Dan Clipca <47941658+virtumonde@users.noreply.github.com>
2019-07-27 14:25:32 +0300

    Add files via upload

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,22 @@
-import * as P5 from 'p5';
-	var customWidth = 1920;
-	var customHeight = 1080;
-	var playerXvelocity =  0;
-	var playerYvelocity = 0;
-	var playerXpos = customWidth/2;
-  	var playerYpos = customHeight/2;
-	var imageAnimationNumberLeft = 0;
-	var imageAnimationNumberRight = 0;
-	var imageAnimationNumberIntLeft = 0;
-	var imageAnimationNumberIntRight = 0;
+import * as P5 from 'p5'
+	//Declaring the variables
+	var customWidth = 1920
+	var customHeight = 1080
+	var playerXvelocity =  0
+	var playerYvelocity = 0
+	var playerXpos = customWidth/2
+  	var playerYpos = customHeight/2
+	var imageAnimationNumberLeft = 0
+	var imageAnimationNumberRight = 0
+	var imageAnimationNumberIntLeft = 0
+	var imageAnimationNumberIntRight = 0
 
 let draw = (p5) => {
   p5.setup = () => {
     p5.createCanvas(innerWidth, innerHeight);
     p5.frameRate(60);
   };
-	
+	// Importing the images
   	var playerFrame00Right = p5.loadImage('https://i.ibb.co/JyXQTwk/player-Frame00.png');
 	var playerFrame01Right = p5.loadImage('https://i.ibb.co/m5Hkkw4/player-Frame01.png');
 	var playerFrame02Right = p5.loadImage('https://i.ibb.co/kHwpjRj/player-Frame02.png');
@@ -29,11 +30,14 @@ let draw = (p5) => {
 	var gameBackground = p5.loadImage('https://i.ibb.co/8jJ3ps2/background.png');
 
   p5.draw = () => {
+
 // Moving the origin coordinates so that the player will move relative to the world
-p5.translate(-playerXpos + innerWidth/2, -playerYpos + innerHeight/2);
+p5.translate(-playerXpos + innerWidth/2, -playerYpos + innerHeight/2)
+
 // X coordinates of the background images
-var xPosBackgroundSceneArray = [-1920, 0, 1920];
-// Draw background
+var xPosBackgroundSceneArray = [-1920, 0, 1920]
+
+// Draw the background as the player is moving
 if (playerYpos > customHeight/2)
 {
 	p5.background(81, 145, 160);
@@ -56,67 +60,62 @@ for (var i = 1; -500*i > playerXpos; i++) {
 for (var i = 0; i < xPosBackgroundSceneArray.length; i++) {
 p5.image(gameBackground, xPosBackgroundSceneArray[i], 0);
 }
+
 // Key logic
 	var keyReceptor = function(event) {
 	var keyState = (event.type === 'keydown')?true:false;
 	if (keyState == true) {
 	// W key logic
-		if (event.keyCode === 87) {
-			playerYvelocity -= 10;
+		if (event.keyCode === 87 && playerYvelocity >= -5 && playerYvelocity <= 5) {
+			playerYvelocity -= 0.1;
 			playerJumps = true;
 			playerIsRight = false;
 			playerIsLeft = false;
 			keyState == false;
 		}
 		
-		if (playerYvelocity < -10) {
-			playerYvelocity = -10;
+		if (playerYvelocity < -5) {
+			playerYvelocity = -5;
 		}
 
-		if (playerYvelocity > 15) {
-			playerYvelocity = 15;
+		if (playerYvelocity > 5) {
+			playerYvelocity = 5;
 		}
 
 	// A key logic
-		if (event.keyCode === 65) {
-			playerXvelocity -= 10;
+		if (event.keyCode === 65 && playerXvelocity >= -5) {
+			playerXvelocity -= 0.1;
 			playerIsLeft = true;
 			playerIsRight = false;
 			playerJumps = false;
 			keyState == false;
 		}
-
-		if (playerXvelocity < -10) {
-			playerXvelocity = -10;
-		}
 	// D key logic
-		if (event.keyCode === 68) {
-			playerXvelocity += 10;
+		if (event.keyCode === 68 && playerXvelocity <= 5) {
+			playerXvelocity += 0.1;
 			playerIsRight = true;
 			playerIsLeft = false;
 			playerJumps = false;
 			keyState = false;
 		}
-		if (playerXvelocity > 10) {
-			playerXvelocity = 10;
-		}
 }
 	};
-
-	// Physics
-	// Every frame the velocity (pixel increment per frame) is added
+	// Velocity
 	playerXpos += playerXvelocity;
 	playerYpos += playerYvelocity;
+
 	// Gravity
-	playerYvelocity += 0.5;
-	// Return the player to the top if he falls down
-	if (playerYpos > customHeight) {
+	playerYvelocity += 0.1;
+
+	// Teleport player to the top if he falls
+	if (playerYpos > customHeight * 2) {
 	playerYpos = -customHeight/4;
 	playerYvelocity = 0;
 	}
+
 	// Friction
-	playerXvelocity *= 0.999;
-	playerYvelocity *= 0.9;
+	playerXvelocity *= 0.9999;
+	playerYvelocity *= 0.9999;
 
 // Animation 
 	  
@@ -167,10 +166,8 @@ if (playerYvelocity < 0 && playerXvelocity > 0) {
 };
 
 // Detect key release
-
 	window.addEventListener('keydown', keyReceptor);
 	window.addEventListener('keyup', keyReceptor);
-
   };
 };
 
